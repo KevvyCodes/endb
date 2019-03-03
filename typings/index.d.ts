ede0453318c7d3ebbd864eb314b0fe9afd5f133c
chroventer <chroventer@gmail.com>
2019-03-03 20:11:21 +0400

    fix: remove typing directory from .gitignore

---
+++ typings/index.d.ts
@@ -0,0 +1,25 @@
+declare module 'endb' {
+    export const version: string;
+    export class Database {
+        constructor(options?: object);
+        public name: string;
+        public memory: boolean;
+        public timeout: number;
+        public db: object;
+
+        public add(key: string | number, value: number): number;
+        public backup(name: string): void;
+        private _check(): void;
+        public close(): void;
+        public delete(key: string | number): boolean;
+        public deleteAll(): boolean;
+        public find(prefix: string | number): object; 
+        public get(key: string | number): string | number | object;
+        public getAll(): object[];
+        public has(key: string | number): boolean;
+        public row2Obj(rows: Array<any>): void;
+        public set(key: string | number, value: string | number | object): object;
+        public subtract(key: string | number, value: number): number;
+        private _validateOptions(options: object): void;
+    }
+}
